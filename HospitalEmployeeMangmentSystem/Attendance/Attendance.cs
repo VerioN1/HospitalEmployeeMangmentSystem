18d958066276d4a27b41f5db429cdc7bffc56ddf
VerioN1 <alonbarel221@gmail.com>
2021-06-26 14:48:27 +0300

    Attendance Initail Class design

---
+++ HospitalEmployeeMangmentSystem/Attendance/Attendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalEmployeeMangmentSystem.Attendance
+{
+    public class Attendance : IAttendance
+    {
+        public string Attendance_EmployeeId => throw new NotImplementedException();
+
+        public int CurrentMonthAttendedHoursAmount => throw new NotImplementedException();
+
+        public void CheckIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetEmployeeMonthHoursAttendance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HospitalEmployeeMangmentSystem/Attendance/IAttendance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalEmployeeMangmentSystem.Attendance
+{
+    interface IAttendance
+    {
+        string Attendance_EmployeeId { get; }
+        int CurrentMonthAttendedHoursAmount { get; }
+
+        void CheckIn();
+        void CheckOut();
+        void GetEmployeeMonthHoursAttendance();
+    }
+}
